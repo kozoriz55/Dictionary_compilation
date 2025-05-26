3b95080c3d890b5aad8ff5548febccc8b77e3f0a
kozoriz55 <82938786+kozoriz55@users.noreply.github.com>
2025-05-26 22:32:17 +0300

    corpus_to_dict.py

--- corpus_to_dict.py
+++ corpus_to_dict.py
@@ -15,12 +15,12 @@ def split_chinese_text(text, dictionary):
 #	return ''.join(words[i] if words[i].upper() in SYMBOLS and words[i + 1].upper() in SYMBOLS else words[i] + " " for i in range(len(words) - 1))
 	return ''.join(w if w.upper() in SYMBOLS and next_w.upper() in SYMBOLS else w + " " for w, next_w in zip(words, words[1:]))
 
-with open("C:\\Users\\davin\\Desktop\\mil_corpus.txt", 'rt', encoding='utf-8') as f1:
+with open("mil_corpus.txt", 'rt', encoding='utf-8') as f1:
 	chinese_text = f1.read()		  
-with open("C:\\Users\\davin\\Desktop\\zh_orpho.txt", 'r', encoding='utf-8') as f:
+with open("zh_orpho.txt", 'r', encoding='utf-8') as f:
 	chinese_dictionary = {word.split('+')[0] for word in f.readlines()}
 chinese_words = split_chinese_text(chinese_text, chinese_dictionary)
-with open("C:\\Users\\davin\\Desktop\\mil_corpus_s_.txt", 'wt', encoding='utf-8') as f2:
+with open("mil_corpus_s_.txt", 'wt', encoding='utf-8') as f2:
 	f2.write(chinese_words)
 print("файл успішно записаний")
 
@@ -33,5 +33,5 @@ for слово in список_слів:
 	my_dict.setdefault(слово, 0)
 	my_dict[слово] = my_dict[слово] + 1
 sorted_dict = dict(sorted(my_dict.items(), key=lambda x: x[1], reverse=True))
-with open("C:\\Users\\davin\\Desktop\\mil_corpus_dict.txt", 'w', encoding='utf-8') as json_file:
+with open("mil_corpus_dict.txt", 'w', encoding='utf-8') as json_file:
 	json.dump(sorted_dict, json_file, ensure_ascii=False)
